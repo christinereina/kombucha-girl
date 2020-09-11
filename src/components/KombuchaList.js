fc1d6e64e43fdfe95a0d272307aeda6e2ff83a7c
blacchoy@gmail.com <blacchoy@gmail.com>
2020-09-11 16:32:04 -0700

    add to kombuchalist

---
+++ src/components/KombuchaList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Kombucha from "./Kombucha";
+import PropTypes from "prop-types";
+
+function KombuchaList(props){
+  return (
+  <React.Fragment>
+    {props.kombuchaList.map(kombucha) =>
+    <Kombucha
+      whenKombuchaClicked = { props.onKombuchaSelection }
+      name={kombucha.name}
+      brand={kombucha.brand}
+      price={kombucha.price}
+      flavor={kombucha.flavor}
+      id={kombucha.id}
+      key={kombucha.id}/>
+    )}
+  </React.Fragment>
+  );
+}
+
+KombuchaList.propTypes = {
+  kombuchaList: PropTypes.array,
+  onKombuchaSelection: PropTypes.func
+};
+
+export default KombuchaList;
