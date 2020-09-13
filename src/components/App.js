0315913dec88789ed610044ee78e10c8279e7d46
blacchoy@gmail.com <blacchoy@gmail.com>
2020-09-12 22:33:32 -0700

    add reusable form

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,8 @@ import './App.css';
 function App() {
   return (
     <React.Fragment>
-      <Container>
         <Header />
-      </Container>
+        <KombuchaControl />
     </React.Fragment>
   );
 }
