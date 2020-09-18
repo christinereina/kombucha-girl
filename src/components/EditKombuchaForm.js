0878031f8d57e93df2444621393b6ab711965fea
blacchoy@gmail.com <blacchoy@gmail.com>
2020-09-18 09:15:02 -0700

    add pre-fill default values to editform

--- src/components/EditKombuchaForm.js
+++ src/components/EditKombuchaForm.js
@@ -3,23 +3,35 @@ import PropTypes from "prop-types";
 import ReusableForm from "./ReusableForm";
 
 function EditKombuchaForm(props){
-  const { kombucha } = props;
+  const { kombucha, onEditKombucha } = props;
 
   function handleEditKombuchaFormSubmission(event) {
-    event.preventDefault();
-    props.onEditKombucha({name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, flavor: event.target.flavor.value, quantity: event.target.quantity.value, id: kombucha.id});
+    onEditKombucha({
+      name: event.target.name.value, 
+      brand: event.target.brand.value, 
+      price: event.target.price.value, 
+      flavor: event.target.flavor.value, 
+      quantity: event.target.quantity.value, 
+      id: kombucha.id
+    });
   }
 
   return (
     <React.Fragment>
-      <ReusableForm 
-        formSubmissionHandler={handleEditKombuchaFormSubmission} 
-        buttonText="Update Kombucha" />
-    </React.Fragment>
+      <ReusableForm
+          formSubmissionHandler={handleEditKombuchaFormSubmission}
+          buttonText='Update Kombucha'
+          defaultName={kombucha.name}
+          defaultBrand={kombucha.brand}
+          defaultPrice={kombucha.price}
+          defaultFlavor={kombucha.flavor}
+          defaultQuantity={kombucha.quantity} />
+  </React.Fragment>
   );
 }
 
 EditKombuchaForm.propTypes = {
+  kombucha: PropTypes.object,
   onEditKombucha: PropTypes.func
 };
 
--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -15,19 +15,19 @@ function ReusableForm(props){
         <Col md={{ span: 6, offset: 3 }}>
           <Form onSubmit={props.formSubmissionHandler}>
             <Form.Group>
-              <Form.Control type='text' name='name' placeholder='Name' />
+              <Form.Control type='text' name='name' placeholder='Name' defaultValue={props.defaultName} />
             </Form.Group>  
             <Form.Group>
-              <Form.Control type='text' name='brand' placeholder='Brand' />
+              <Form.Control type='text' name='brand' placeholder='Brand' defaultValue={props.defaultBrand} />
             </Form.Group>
             <Form.Group>  
-              <Form.Control type='text' name='price' placeholder="Price" />
+              <Form.Control type='text' name='price' placeholder="Price" defaultValue={props.defaultPrice}/>
             </Form.Group>
             <Form.Group>
-              <Form.Control type='text' name='flavor'placeholder="Flavor" />
+              <Form.Control type='text' name='flavor'placeholder="Flavor"   defaultValue={props.defaultFlavor}/>
             </Form.Group>
             <Form.Group>  
-              <Form.Control type='number'name='quantity' placeholder="quantity" />
+              <Form.Control type='number'name='quantity' placeholder="quantity"   defaultValue={props.defaultQuantity}/>
             </Form.Group>
             <Button variant='info' type='submit'>{props.buttonText}</Button>
           </Form>
@@ -40,7 +40,12 @@ function ReusableForm(props){
 
 ReusableForm.propTypes = {
   formSubmissionHandler: PropTypes.func,
-  buttonText: PropTypes.string
+  buttonText: PropTypes.string,
+  defaultName: PropTypes.string,
+  defaultPBrand: PropTypes.string,
+  defaultPrice: PropTypes.string,
+  defaultQuantity: PropTypes.string,
+  defaultFlavor: PropTypes.string
 };
 
 export default ReusableForm;
